79832625b64ebbc3113df573186bba7a79beec09
Jevon Liau <jevonliau@gmail.com>
2021-08-16 15:13:50 +0800

    Changed code for convertindextouvspace (Fixed it abit idk)

--- Library/Source/GameControl/Settings.cpp
+++ Library/Source/GameControl/Settings.cpp
@@ -79,8 +79,14 @@ float CSettings::ConvertIndexToUVSpace(const AXIS sAxis, const int iIndex, const
 glm::vec2 CSettings::ConvertIndexToUVSpace(const glm::vec2 pos)
 {
 	glm::vec2 output = pos;
-	output.x = (output.x - (0.5 * NUM_TILES_XAXIS)) / (0.5 * NUM_TILES_XAXIS);
-	output.y = (output.y - (0.5 * NUM_TILES_YAXIS)) / (0.5 * NUM_TILES_YAXIS);
+
+	//ORIGINAL ONE
+	//output.x = (output.x - (0.5 * NUM_TILES_XAXIS)) / (0.5 * NUM_TILES_XAXIS);
+	//output.y = (output.y - (0.5 * NUM_TILES_YAXIS)) / (0.5 * NUM_TILES_YAXIS);
+
+	//NEW ONE
+	output.x = (((pos.x + 0.5) / NUM_TILES_XAXIS) * 2) - 1;
+	output.y = (((pos.y + 0.5)/ NUM_TILES_YAXIS) * 2) - 1;
 
 	return output;
 }
