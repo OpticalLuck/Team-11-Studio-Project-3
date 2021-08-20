546137c056921ae5c518bf2c8a175b5bc1ba0c1d
jeryl lah <56944877+OpticalLuck@users.noreply.github.com>
2021-08-20 17:31:01 +0800

    Update Player2D.cpp

--- App/Source/Scene2D/Player2D.cpp
+++ App/Source/Scene2D/Player2D.cpp
@@ -361,8 +361,6 @@ void CPlayer2D::Update(const double dElapsedTime)
 		}
 	}
 
-	cout << cPhysics2D.GetVelocity().x << ", " << cPhysics2D.GetVelocity().y << " | " << cPhysics2D.GetboolGrounded() << endl;
-	
 	
 	//BOUNDARY CHECK
 	//if (vTransform.y > cMap2D->GetLevelRow() - 1 || vTransform.x > cMap2D->GetLevelCol() - 1 || vTransform.y < -1 || vTransform.x < -1)
